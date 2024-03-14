147e382a04cd00ae93ba174ffbac95a224088743
Shabab <s7abab2k1@gmail.com>
2024-03-14 14:26:00 +0530

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hello">
+    <h1>Counter App</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Counter App",
+    data(){
+      return count:0
+    }
+  }
+</script>
