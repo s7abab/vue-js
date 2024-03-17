f843a12d180ec9ee52602224f4f54e73edca141d
Shabab <s7abab2k1@gmail.com>
2024-03-17 22:05:53 +0530

    event handling

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,114 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<!-- 1 -->
+<!-- conditional rendering -->
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld  />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<!-- <template>
+  <h1>Header</h1>
+  <h1 v-if="num===0" >Number = {{ num }}</h1>
+  <h1 v-else >Number = -1</h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      num:0
+    }
   }
 }
-</style>
+</script>
+
+
+<style>
+.italic {
+  font-style: italic;
+  color: blueviolet;
+}
+</style> -->
+
+
+<!-- 2 -->
+<!-- rendering list -->
+
+<!-- <template>
+  <h1>This is array of objects</h1>
+  <div v-for="person in people" :key="person.name">
+    <div>{{ person.name }} | {{ person.age }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      people: [{ name: 'John', age: 30 }, { name: 'Mary', age: 29 }, { name: 'Superman', age: 120 }]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style> -->
+
+<!-- 3 -->
+<!-- methods -->
+
+<!-- <template>
+  <h1>{{ add()}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  methods:{
+    add(){
+      return 5+5
+    }
+  }
+}
+</script> -->
+
+<!-- 4  -->
+<!-- event handling -->
+<!-- <template>
+  <h1>Counter</h1>
+  <h1>{{ count }}</h1>
+  <div class="flex_container">
+    <button v-on:click="increment()">+</button>
+    <button v-on:click="decrement()">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--
+    }
+  }
+}
+</script>
+
+<style>
+.flex_container {
+  display: flex;
+  gap: 10px;
+  background-color: blue;
+}
+</style> -->
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="hello">
-    <h1>Counter App</h1>
-  </div>
+
 </template>
 
 <script>
-  export default {
-    name: "Counter App",
-    data(){
-      return count:0
-    }
-  }
+
 </script>
